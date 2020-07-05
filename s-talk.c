15466cfa57503fa329f5b19aad0c9cb9dbfc8d58
Kevin <kevin.chensu@gmail.com>
2020-07-04 22:20:51 -0700

    testing arguments

--- s-talk.c
+++ s-talk.c
@@ -17,12 +17,20 @@ int sockfd;
 
 int main(int argc, char **argv){
 
+    // passing arguments to the program (ie. ./s-talk[]
+    for(int i = 0; i < argc; i++){
+        printf("Argv %d: %s\n", i, argv[i]);
+    }
+
     sockfd = socket(PF_INET, SOCK_DGRAM, 0);
 
     my_addr.sin_family = AF_INET;
     my_addr.sin_port = htons(2200); // ports below 1024 are reserve, can use any port up to 65535
     my_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // clear my_addr pointer
     memset(&my_addr, 0, sizeof(my_addr));
+    
     if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) == -1){
         
         // check if is failed
